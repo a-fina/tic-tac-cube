10ec0cd7fa0305313fd1e43e10fc71b06c18e870
Alessio Finamore <mov86877@adobe.com>
2017-09-11 15:44:03 +0200

    AJ2-CLI: removed body-parser

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 var express = require("express");
-var bodyParser = require("body-parser");
-
 
 var app = express();
-app.use(bodyParser.json());
 
 // Create link to Angular build directory
 var distDir = __dirname + "/dist/";
